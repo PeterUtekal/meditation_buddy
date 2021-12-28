236ab182bbc4015d6b7b1bdaedfa82ad34925849
vanessaingeli <vanesahlavacova@gmail.com>
2021-12-28 23:47:57 +0000

    dzopraviladzvanesska

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          dzahoj <a href="https://nextjs.org">vanesska</a>
         </h1>
 
         <p className={styles.description}>
